4cca4d90618a1f88eaf390f292a1b0643917d81b
郭广川 <ggc0402@qq.com>
2019-07-22 18:49:49 +0800

    add test cluster

---
+++ examples/src/main/scala/spark/examples/TestCluster.scala
@@ -0,0 +1,21 @@
+package spark.examples
+
+import spark._
+import SparkContext._
+class TestCluster {
+  def getlist() = List("1")
+}
+object TestCluster{
+  def main(args: Array[String]): Unit = {
+    var sc = new SparkContext("spark://127.0.0.1:7077", "Spark shell")
+    var rdd = sc.parallelize(1 to 8, 4)
+    var pairdd = rdd.map(x => (x, x * x))
+    var reducerdd = pairdd.reduceByKey(_ + _)
+    var rrducerdd = reducerdd.reduceByKey(_ + _)
+    var res = rrducerdd.collect()
+    println(res)
+    while (true){
+      Thread.sleep(10);
+    }
+  }
+}
